2d52b8181e83d213285f50c5dc5ea39d6a6edb47
vail <ysywix@gmail.com>
2019-09-24 19:49:13 +0300

    Dummy

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * @fileoverview Lodash to ES6
+ * @author Aleksey Orlov
+ */
+'use strict'
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const requireIndex = require('requireindex')
+
+//------------------------------------------------------------------------------
+// Plugin Definition
+//------------------------------------------------------------------------------
+
+// import all rules in lib/rules
+module.exports.rules = requireIndex(__dirname + '/lib/rules')
+
+
+
---
+++ tests/lib/rules/map.js
@@ -0,0 +1,36 @@
+/**
+ * @fileoverview _.map to Array.map
+ * @author Aleksey Orlov
+ */
+'use strict'
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const
+  rule = require('../../../lib/rules/map'),
+  RuleTester = require('eslint').RuleTester
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester()
+ruleTester.run('map', rule, {
+
+  valid: [
+
+    // give me some code that won't trigger a warning
+  ],
+
+  invalid: [
+    {
+      code: '_.map([])',
+      errors: [{
+        message: 'Fill me in.',
+        type: 'Me too'
+      }]
+    }
+  ]
+})
